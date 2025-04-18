cedd895cf1a3628a6f635aacc1bd8a29c31c942b
Luan Henrique <55123262+Buddy2025@users.noreply.github.com>
2025-04-18 16:18:41 -0300

    Nivel mestre
    Funções (cone, octaedro e cruz)
    Habilidades especiais para as novas funções
    Exibição do tabuleiro com navios e suas posições e áreas limpas

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,10 +4,62 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
+#define TAMANHO_TABULEIRO 10
+
+int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO] = {0}; // Matriz do tabuleiro
+
+// Função para exibir o tabuleiro
+void exibir_tabuleiro() {
+    for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
+        for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+// Nível Mestre - Habilidades Especiais com Matrizes
+
+void aplicar_habilidade_cone(int x, int y) {
+    if (x >= 0 && x + 2 < TAMANHO_TABULEIRO && y - 2 >= 0 && y + 2 < TAMANHO_TABULEIRO) {
+        tabuleiro[x][y] = 1;
+        tabuleiro[x + 1][y] = 1;
+        tabuleiro[x + 1][y - 1] = 1;
+        tabuleiro[x + 1][y + 1] = 1;
+        tabuleiro[x + 2][y] = 1;
+        tabuleiro[x + 2][y - 1] = 1;
+        tabuleiro[x + 2][y + 1] = 1;
+        tabuleiro[x + 2][y - 2] = 1;
+        tabuleiro[x + 2][y + 2] = 1;
+    }
+}
+
+void aplicar_habilidade_octaedro(int x, int y) {
+    if (x - 1 >= 0 && x + 1 < TAMANHO_TABULEIRO && y - 1 >= 0 && y + 1 < TAMANHO_TABULEIRO) {
+        tabuleiro[x][y] = 1;
+        tabuleiro[x - 1][y] = 1;
+        tabuleiro[x + 1][y] = 1;
+        tabuleiro[x][y - 1] = 1;
+        tabuleiro[x][y + 1] = 1;
+        tabuleiro[x - 1][y - 1] = 1;
+        tabuleiro[x - 1][y + 1] = 1;
+        tabuleiro[x + 1][y - 1] = 1;
+        tabuleiro[x + 1][y + 1] = 1;
+    }
+}
+
+void aplicar_habilidade_cruz(int x, int y) {
+    if (x >= 0 && x < TAMANHO_TABULEIRO && y >= 0 && y < TAMANHO_TABULEIRO) {
+        tabuleiro[x][y] = 1;
+        if (x - 1 >= 0) tabuleiro[x - 1][y] = 1;
+        if (x + 1 < TAMANHO_TABULEIRO) tabuleiro[x + 1][y] = 1;
+        if (y - 1 >= 0) tabuleiro[x][y - 1] = 1;
+        if (y + 1 < TAMANHO_TABULEIRO) tabuleiro[x][y + 1] = 1;
+    }
+}
+
 int main() {
     // Nível Novato - Posicionamento dos Navios
-    int tabuleiro[10][10] = {0};
-
     int navio_horizontal[3] = {3, 3, 3};
     int navio_vertical[3] = {3, 3, 3};
 
@@ -40,14 +92,6 @@ int main() {
         }
     }
 
-    // Exibição do tabuleiro
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            printf("%d ", tabuleiro[i][j]);
-        }
-        printf("\n");
-    }
-
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     int navio_diagonal1[3] = {3, 3, 3};
     int navio_diagonal2[3] = {3, 3, 3};
@@ -76,36 +120,14 @@ int main() {
         }
     }
 
-    // Exibição atualizada do tabuleiro após os 4 navios (2 horizontais/verticais e 2 diagonais)
-    printf("\nTabuleiro com 4 navios (incluindo diagonais):\n");
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            printf("%d ", tabuleiro[i][j]);
-        }
-        printf("\n");
-    }
+    // Aplicação das habilidades especiais
+    aplicar_habilidade_cone(2, 6);
+    aplicar_habilidade_octaedro(7, 3);
+    aplicar_habilidade_cruz(5, 5);
 
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    // Exibição do tabuleiro atualizado
+    printf("\nTabuleiro com habilidades aplicadas:\n");
+    exibir_tabuleiro();
 
     return 0;
 }
